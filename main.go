aa9093a2fc757089f5aaca6396c854124617b8a6
lebrancconvas <lebrancconvas@gmail.com>
2023-02-18 14:07:27 +0700

    ✨ feat: create and get api by gorm

--- main.go
+++ main.go
@@ -14,9 +14,18 @@ type SqlLogger struct {
 	logger.Interface
 }
 
+type Test struct {
+	gorm.Model
+	Name string `gorm:"column:test_name;type:varchar(50);unique;default:unknown;not null"`
+}
+
 type Gender struct {
 	ID uint
-	Name string
+	Name string `gorm:"unique;size(10)"`
+}
+
+func (t Test) TableName() string {
+	return "my_test";
 }
 
 func (l SqlLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
@@ -24,20 +33,66 @@ func (l SqlLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql st
 	fmt.Printf("%v\n=====================\n", sql);
 }
 
+var db *gorm.DB;
+
+func CreateGender(name string) {
+	gender := Gender{Name: name};
+	tx := db.Create(&gender);
+	if tx.Error != nil {
+		fmt.Println(tx.Error);
+		return; 
+	}
+	fmt.Println(gender); 
+}
+
+func GetGenders() {
+	genders := []Gender{};
+	tx := db.Order("id").Find(&genders);
+	if tx.Error != nil {
+		fmt.Println(tx.Error);
+		return;
+	}
+	fmt.Println(genders);
+}
+
+func GetGenderByID(id int) {
+	gender := Gender{};
+	tx := db.First(&gender, id);
+	if tx.Error != nil {
+		fmt.Println(tx.Error);
+		return;
+	}
+	fmt.Println(gender);
+}
+
+func GetGenderByName(name string) {
+	gender := Gender{};
+	tx := db.First(&gender, "name=?", name);
+	if tx.Error != nil {
+		fmt.Println(tx.Error);
+		return;
+	}
+	fmt.Println(gender);
+}
+
 func main() {
 	dsn := "root:P@ssw0rd@tcp(0.0.0.0:3306)/goorm_codebangkok?parseTime=true";
 	mysql := mysql.Open(dsn);
-	db, err := gorm.Open(mysql, &gorm.Config{
+	var err error;
+	db, err = gorm.Open(mysql, &gorm.Config{
 		Logger: &SqlLogger{},
-		DryRun: true,
+		DryRun: false,
 	});
 	if err != nil {
 		panic(err);
 	}
 	
-	err = db.Migrator().CreateTable(Gender{});
-	if err != nil {
-		fmt.Println(err); 
-	}
+	// err = db.AutoMigrate(Gender{}, Test{});
+	// if err != nil {
+	// 	fmt.Println(err); 
+	// }
 
+	GetGenders(); 
+	GetGenderByID(2); 
+	GetGenderByName("Male");
 }
